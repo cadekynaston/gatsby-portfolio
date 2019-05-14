c50a5b92060bb8503b74b96d7faa0fcfb49da1d9
Cade Kynaston <cade.kynaston@clearlink.com>
2019-05-14 13:39:46 -0600

    updated imports

--- src/styles/index.js
+++ src/styles/index.js
@@ -1,8 +1,8 @@
 import theme from './theme'
 import Container from './container'
-import Section from './section'
+import Section from './Section'
 import media from './media'
-import GlobalStyles from './globalStyles'
+import GlobalStyles from './GlobalStyles'
 
 export {
   theme,
