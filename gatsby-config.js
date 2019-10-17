bd4fdf91dbbd9b0c7799b63fc98638da2fe4bdf1
Cade Kynaston <cade.kynaston@clearlink.com>
2019-10-17 16:00:17 -0600

    updated favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -40,7 +40,7 @@ module.exports = {
         background_color: `#1E2749`,
         theme_color: `#1E2749`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/ck-logo-light.png`, // This path is relative to the root of the site.
       },
     },
     `gatsby-transformer-json`,
